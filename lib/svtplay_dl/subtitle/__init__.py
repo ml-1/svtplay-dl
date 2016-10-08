db4f097b269b9b62dbc50bac91850fffa62bf80e
Johan Andersson <j@i19.se>
2016-10-08 14:12:08 +0200

    remove is_pyX stuff

--- lib/svtplay_dl/subtitle/__init__.py
+++ lib/svtplay_dl/subtitle/__init__.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import json
 import re
 from svtplay_dl.log import log
-from svtplay_dl.utils import is_py2, is_py3, decode_html_entities, HTTP
+from svtplay_dl.utils import decode_html_entities, HTTP
 from svtplay_dl.utils.io import StringIO
 from svtplay_dl.output import output
 from requests import __build__ as requests_version
@@ -53,7 +53,7 @@ class subtitle(object):
         self.save_file(data, "srt")
         
     def save_file(self, data, subtype):
-        if platform.system() == "Windows" and is_py3:
+        if platform.system() == "Windows":
             file_d = output(self.options, subtype, mode="wt", encoding="utf-8")
         else:
             file_d = output(self.options, subtype, mode="wt")
@@ -64,19 +64,13 @@ class subtitle(object):
         
         
     def raw(self, subdata): 
-        if is_py2:
-            data = subdata.text.encode("utf-8")
-        else:
-            data = subdata.text
+        data = subdata.text
         return data
         
     def tt(self, subdata):
         i = 1
         data = ""
-        if is_py2:
-            subs = subdata.text.encode("utf8")
-        else:
-            subs = subdata.text
+        subs = subdata.text
 
         subdata = re.sub(' xmlns="[^"]+"', '', subs, count=1)
         tree = ET.XML(subdata)
@@ -104,8 +98,7 @@ class subtitle(object):
                 data = tt_text(node, data)
                 data += "\n"
                 i += 1
-        if is_py2:
-            data = data.encode("utf8")
+
         return data
 
     def json(self, subdata):
@@ -114,18 +107,13 @@ class subtitle(object):
         subs = ""
         for i in data:
             subs += "%s\n%s --> %s\n" % (number, timestr(int(i["startMillis"])), timestr(int(i["endMillis"])))
-            if is_py2:
-                subs += "%s\n\n" % i["text"].encode("utf-8")
-            else:
-                subs += "%s\n\n" % i["text"]
+            subs += "%s\n\n" % i["text"]
             number += 1
 
         return subs
 
     def sami(self, subdata):
         text = subdata.text
-        if is_py2:
-            text = text.encode("utf8")
         text = re.sub(r'&', '&amp;', text)
         tree = ET.fromstring(text)
         subt = tree.find("Font")
@@ -143,17 +131,12 @@ class subtitle(object):
                 if int(n) > 0 and i.text:
                     subs += "%s\n" % decode_html_entities(i.text)
 
-        if is_py2:
-            subs = subs.encode('utf8')
         subs = re.sub('&amp;', r'&', subs)
         return subs
 
     def smi(self, subdata):
         if requests_version < 0x20300:
-            if is_py2:
-                subdata = subdata.content
-            else:
-                subdata = subdata.content.decode("latin")
+            subdata = subdata.content.decode("latin")
         else:
             subdata.encoding = "ISO-8859-1"
             subdata = subdata.text
@@ -182,8 +165,6 @@ class subtitle(object):
                 data = text.group(1)
         recomp = re.compile(r'\r')
         text = bad_char.sub('-', recomp.sub('', subs)).replace('&quot;', '"')
-        if is_py2 and isinstance(text, unicode):
-            return text.encode("utf-8")
         return text
 
     def wrst(self, subdata):
@@ -247,8 +228,6 @@ class subtitle(object):
                 srt += sub.strip()
                 srt+="\n"
         srt = decode_html_entities(srt)
-        if is_py2:
-            return srt.encode("utf-8")
         return srt
 
 
